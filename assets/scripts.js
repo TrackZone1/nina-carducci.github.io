4a5fe6516f4e9d27388a9b0e98746890df50028f
TrackZone <48413426+TrackZoneV2@users.noreply.github.com>
2023-06-26 17:18:08 +0200

    95 - 100 - 100 - 100

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
+$(document).ready(function () {
+    $(".gallery").mauGallery({
         columns: {
             xs: 1,
             sm: 2,
             md: 3,
             lg: 3,
-            xl: 3
+            xl: 3,
         },
         lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
+        lightboxId: "myAwesomeLightbox",
         showTags: true,
-        tagsPosition: 'top'
+        tagsPosition: "top",
     });
 });
